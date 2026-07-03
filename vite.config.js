96e06900d5df81e7893f5e5c3e5aa74febdc12cc
Josip Lopez <josiplopez23@gmail.com>
2026-07-02 23:07:44 -0500

    update PWA icons from realfavicongenerator

--- vite.config.js
+++ vite.config.js
@@ -18,24 +18,24 @@ export default defineConfig({
         orientation: 'portrait',
         scope: '/',
         start_url: '/',
-        icons: [
-          {
-            src: 'snoutt-icon.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'snoutt-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'snoutt-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+       icons: [
+  {
+    src: 'web-app-manifest-192x192.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: 'web-app-manifest-512x512.png',
+    sizes: '512x512',
+    type: 'image/png',
+  },
+  {
+    src: 'web-app-manifest-512x512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any maskable',
+  },
+],
       },
     }),
   ],
